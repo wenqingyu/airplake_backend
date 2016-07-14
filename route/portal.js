58f322b94c3f59dbc593537cbcf2245155981267
X.C <119136016@qq.com>
2016-07-14 16:41:09 +0800

    完成所有接口

--- route/portal.js
+++ route/portal.js
@@ -1,13 +1,17 @@
 'use strict'
 exports.autoroute = {
     get: {
-
+        '/api/v1/portals': list, //获取列表
+        '/api/v1/portals/:id': detail //明细
     },
     post: {
         '/api/v1/portals': add //新增服务商入口
     },
     put: {
         '/api/v1/portals/:id': update //更新服务商入口
+    },
+    delete: {
+        '/api/v1/portals/:id': del //删除服务商入口
     }
 };
 var co = require('co');
@@ -55,6 +59,50 @@ function update(req, res) {
         sql = gbObj.mysql.makeSQLUpdate('portal', req.body, { id: id });
         result = yield gbObj.pool.queryAsync(sql);
         res.apiSuccess();
+    }).catch(function (err) {
+        logger.error(err);
+        res.apiError(er);
+    })
+}
+
+/**
+ * 删除服务商入口
+ */
+function del(req, res) {
+    co(function* () {
+        let id = req.params.id;
+        let sql = gbObj.mysql.makeSQLDelete('portal', { id: id });
+        let result = yield gbObj.pool.queryAsync(sql);
+        res.apiSuccess();
+    }).catch(function (err) {
+        logger.error(err);
+        res.apiError(er);
+    })
+}
+
+/**
+ * 获取列表
+ */
+function list(req, res) {
+    co(function* () {
+        let sql = gbObj.mysql.makeSQLSelect('portal', ['*']);
+        let result = yield gbObj.pool.queryAsync(sql);
+        res.apiSuccess(result);
+    }).catch(function (err) {
+        logger.error(err);
+        res.apiError(er);
+    })
+}
+
+/**
+ * 明细
+ */
+function detail(req, res) {
+    co(function* () {
+        let id = req.params.id;
+        let sql = gbObj.mysql.makeSQLSelect('portal', ['*'], { id: id });
+        let result = yield gbObj.pool.queryAsync(sql);
+        res.apiSuccess(result);
     }).catch(function (err) {
         logger.error(err);
         res.apiError(er);
